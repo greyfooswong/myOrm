4ae7b56a4eefd44f3bfc66f485e8db95bd5e6212
southli <961065551@qq.com>
2021-11-06 17:23:47 +0800

    封装处数据库的统一接口，屏蔽上层，避免过度暴露。同时提升可扩展性。

--- session/raw.go
+++ session/raw.go
@@ -14,10 +14,27 @@ import (
 type Session struct {
 	db       *sql.DB
 	dialect  dialect.Dialect
+	tx       *sql.Tx
 	refTable *schema.Schema
 	clause   clause.Clause
 	sql      strings.Builder
 	sqlVars  []interface{}
+}
+
+type CommonDB interface {
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
+var _ CommonDB = (*sql.DB)(nil)
+var _ CommonDB = (*sql.Tx)(nil)
+
+func (s *Session) DB() CommonDB {
+	if s.tx != nil {
+		return s.tx
+	}
+	return s.db
 }
 
 // New creates a instance of Session
@@ -33,11 +50,6 @@ func (s *Session) Clear() {
 	s.sql.Reset()
 	s.sqlVars = nil
 	s.clause = clause.Clause{}
-}
-
-// DB returns *sql.DB
-func (s *Session) DB() *sql.DB {
-	return s.db
 }
 
 // Exec raw sql with sqlVars
